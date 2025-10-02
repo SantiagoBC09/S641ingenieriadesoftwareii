75aa51a58d3b64a90809810194ce1f87cbfb0477
SantiagoBC09 <santiagobalanta@estudiante.uniajc.edu.co>
2025-10-02 04:38:15 +0000

    creacion de clases para el patron builder

---
+++ patronBuilderS641/PostScriptConvert.java
@@ -0,0 +1,16 @@
+public class PostScriptConvert extends Converter {
+  
+  public void makeLine(){
+        
+       System.out.println("Línea con PostScript");
+    }
+    public void makeParagraph(){
+
+       System.out.println("Párrafo con PostScript");
+    }
+   public void makeTable(){
+
+       System.out.println("Tabla con PostScript");
+    }  
+
+}
